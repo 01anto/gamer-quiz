d32a06d0323521ff87329b72df36f0d134f1869f
01anto <01anto@live.ie>
2022-10-24 16:59:05 +0000

    Update get_user_data function
    Print statement removed

--- run.py
+++ run.py
@@ -19,7 +19,6 @@ def get_user_data():
     """
     username = input("Please enter your username --> ")
     password = input("Please enter your password --> ")
-    print(f"Username is: {username}\n Password is {password}")
     return username, password
 
 
