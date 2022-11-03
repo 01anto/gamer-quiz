773f6079be52279a0c3b25c3215ff28a33bb18f9
01anto <01anto@live.ie>
2022-11-03 19:29:41 +0000

    Remove unecessary code

--- run.py
+++ run.py
@@ -1,17 +1,4 @@
 import time
-import gspread
-from google.oauth2.service_account import Credentials
-
-SCOPE = [
-    "https://www.googleapis.com/auth/spreadsheets",
-    "https://www.googleapis.com/auth/drive.file",
-    "https://www.googleapis.com/auth/drive"
-    ]
-
-CREDS = Credentials.from_service_account_file('creds.json')
-SCOPED_CREDS = CREDS.with_scopes(SCOPE)
-GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
-SHEET = GSPREAD_CLIENT.open('gamer_quiz')
 
 
 class Question:
@@ -102,23 +89,12 @@ def get_user_data():
     return username, password
 
 
-def update_user_details_worksheet(data):
-    """
-    Update user_details worksheet, Add a new row for username and password
-    """
-    print("Updating user_details worksheet")
-    user_details_worksheet = SHEET.worksheet("user_details")
-    user_details_worksheet.append_row(data)
-    print("user_details worksheet updated successfully.\n")
-
-
 def main():
     """
     Run all gamer-quiz functions
     """
     gamer_quiz_logo()
     data = get_user_data()
-    update_user_details_worksheet(data)
     ask_question()
 
 
